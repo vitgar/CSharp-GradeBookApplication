767aa8a4270e0b2800f782a94a38e03f6cefbfe1
vitgar <vitgar@yahoo.com>
2018-10-20 10:17:47 -0500

    19.1

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -17,7 +17,7 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
-                throw new InvalidOperationException("Ranked grading requires at least 5 studnets.");
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
 
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e=>e.AverageGrade).ToList();
@@ -28,7 +28,7 @@ namespace GradeBook.GradeBooks
                 return 'B';
             else if (grades[(threshold * 3) - 1] <= averageGrade)
                 return 'C';
-            else if (grades[threshold * 4 - 1] <= averageGrade)
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
                 return 'D';
             else
                 return 'F';
